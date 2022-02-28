14937109edc51c26642edaf54e40352c000c9e86
brandib222 <brandi.danielle.ball@gmail.com>
2022-02-28 13:39:25 -0500

    set up env index and server

---
+++ api/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const authRouter = require("./auth/auth-router.js");
+const usersRouter = require("./users/users-router.js");
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
+
+server.use((req, res, next) => {
+  res.status(500).json({
+    message: 'Error, please try again'
+  });
+});
+
+module.exports = server;
