9fbc2a9c69e2d39976fb5be41093bae45a6e25be
brandib222 <brandi.danielle.ball@gmail.com>
2022-02-28 14:38:16 -0500

    built users router

---
+++ api/users/users-router.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+const Users = require('./users-model')
+
+router.get('/', (req, res, next) => {
+    Users.find()
+    .then(users => {
+        res.json(users)
+    }).catch(next);
+});
+
+router.get('/:user_id', (req, res, next) => {
+    Users.findById(req.params.user_id)
+    .then(user => {
+        res.json(user)
+    }).catch(next);
+})
