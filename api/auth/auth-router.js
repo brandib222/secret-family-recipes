533fddb6eeca3d0798a48c2b48799f32f26c9944
brandib222 <brandi.danielle.ball@gmail.com>
2022-02-28 14:01:14 -0500

    did knex init and built db config

---
+++ api/auth/auth-router.js
@@ -0,0 +1,8 @@
+// const bcrypt = require('bcryptjs')
+// const jwt = require('jsonwebtoken')
+
+// const router = require('express').Router()
+// const User = require('../users/users-model')
+
+// const { JWT_SECRET } = require('../secrets/index')
+
