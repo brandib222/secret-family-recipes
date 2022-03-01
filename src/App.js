e573397465510f339f09294f33518d09c61c0565
brandib222 <brandi.danielle.ball@gmail.com>
2022-03-01 16:25:24 -0500

    going back

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import LandingPage from './components/LandingPage';
 //import AddPost from './components/AddPost';
-import Login from './components/Login';
+//import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Login />
+     
       <LandingPage />
       <Footer />
     </div>
