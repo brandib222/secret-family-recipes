baacc60a8de02e6d34f03dd3de68180d85f798dc
brandib222 <brandi.danielle.ball@gmail.com>
2022-03-22 11:29:32 -0400

    taking stock of what still needs to be done

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,7 @@
 // uses axios with auth to authorize a user to these paths
 
-// middlwareeee
+// what still needs to be done
+    //middle ware obvi
+    // make sure login works
+    // make sure post router and models work
+    // backend is *****DONE*******
